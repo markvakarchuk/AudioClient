ee26edaa3aeb1ed9719f789c880f5818c0a83227
markvakarchuk <vakaro12.mark@gmail.com>
2022-02-25 18:06:00 -0500

    another save before I chage it up

--- App.js
+++ App.js
@@ -1,88 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import {Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import {Image, StyleSheet, Text, TouchableOpacity, View, Button } from 'react-native';
 import * as Sharing from 'expo-sharing'; 
 import logo from './assets/icon.png'; 
 import * as ImagePicker from 'expo-image-picker';
 import uploadToAnonymousFilesAsync from 'anonymous-files';
+import { Audio } from 'expo-av';
 //import MusicControl from 'react-native-music-control'
 //import Button from 'react-bootstrap/Button';
 //import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 export default function App() {
-  const [selectedImage, setSelectedImage] = React.useState(null);
+  const [sound, setSound] = React.useState();
 
-  let openImagePickerAsync = async () => {
-    let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
-
-    if (permissionResult.granted === false) {
-      alert("Permission to access camera roll is required!");
-      return;
-    }
-
-    let pickerResult = await ImagePicker.launchImageLibraryAsync();
-    
-    if (pickerResult.cancelled === true) {
-      return;
-    }
-
-    if (Platform.OS === 'web') {
-      let remoteUri = await uploadToAnonymousFilesAsync(pickerResult.uri);
-      setSelectedImage({ localUri: pickerResult.uri, remoteUri });
-    } else {
-      setSelectedImage({ localUri: pickerResult.uri, remoteUri: null });
-    } 
-
-    setSelectedImage({ localUri: pickerResult.uri });
-  };
-
-  let openShareDialogAsync = async () => {
-    if (Platform.OS === 'web') {
-      alert(`The image is available for sharing at: ${selectedImage.remoteUri}`);
-      return;
-    }
-
-    await Sharing.shareAsync(selectedImage.localUri);
-  }; 
-
-  if (selectedImage !== null) {
-    return (
-      <View style={styles.container}>
-        <Image source={{ uri: selectedImage.localUri }} style={styles.thumbnail}/>
-        <TouchableOpacity onPress={openShareDialogAsync} style={styles.button}>
-          <Text style={styles.buttonText}>Share this photo</Text>
-        </TouchableOpacity>
-      </View>
+  async function playSound() {
+    console.log('Loading Sound');
+    const { sound } = await Audio.Sound.createAsync(
+       require('./assets/audio.mp3')
     );
+    setSound(sound);
+
+    console.log('Playing Sound');
+    await sound.playAsync(); 
   }
-  if (false){
+
+
+  React.useEffect(() => {
+    return sound
+      ? () => {
+          console.log('Unloading Sound');
+          sound.unloadAsync(); }
+      : undefined;
+  }, [sound]);
+
+
   return (
     <View style={styles.container}>
-      <Image source={{ uri: "https://i.imgur.com/TkIrScD.png" }} style={styles.icon} />
-
-      <Text style={styles.instructions}>
-        To share a photo from your phone with a friend, just press the button below!
-      </Text>
-
-
-      <TouchableOpacity onPress={openImagePickerAsync} style={styles.button}>
-        <Text style={styles.buttonText}>Pick a photo</Text>
-      </TouchableOpacity>
-      <Button variant="primary">Primary</Button>{styles.button}
+      <Button title="Play Sound" onPress={playSound} />
     </View>
   );
-  }
-
-  return(
-    <View style={styles.container}>
-      <TouchableOpacity style={styles.play}>
-        <Text style={styles.controlText}>''</Text>
-      </TouchableOpacity>
-    </View>
-  )
-
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
