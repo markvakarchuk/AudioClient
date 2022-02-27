9391a9bf08596c4c6f3eb625a26dc3eb79ee914a
markvakarchuk <vakaro12.mark@gmail.com>
2022-02-27 15:10:26 -0500

    first streaming version

--- App.js
+++ App.js
@@ -1,15 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import {Image, StyleSheet, Text, TouchableOpacity, View, Button } from 'react-native';
-import * as Sharing from 'expo-sharing'; 
-import logo from './assets/icon.png'; 
-import * as ImagePicker from 'expo-image-picker';
-import uploadToAnonymousFilesAsync from 'anonymous-files';
+import * as React from 'react';
+import { Text, View, StyleSheet, Button } from 'react-native';
 import { Audio } from 'expo-av';
-//import MusicControl from 'react-native-music-control'
-//import Button from 'react-bootstrap/Button';
-//import 'bootstrap/dist/css/bootstrap.min.css';
-
 
 export default function App() {
   const [sound, setSound] = React.useState();
@@ -17,14 +8,14 @@ export default function App() {
   async function playSound() {
     console.log('Loading Sound');
     const { sound } = await Audio.Sound.createAsync(
-       require('./assets/audio.mp3')
+      //{require: './assets/audio.mp3'}
+      {uri: 'http://streaming.livespanel.com:20000/live' },
     );
     setSound(sound);
 
     console.log('Playing Sound');
     await sound.playAsync(); 
   }
-
 
   React.useEffect(() => {
     return sound
@@ -34,7 +25,6 @@ export default function App() {
       : undefined;
   }, [sound]);
 
-
   return (
     <View style={styles.container}>
       <Button title="Play Sound" onPress={playSound} />
@@ -42,45 +32,11 @@ export default function App() {
   );
 }
 
-
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
-  },
-  icon: { 
-    width: 305, 
-    height: 159 
-  },
-  instructions: {
-    color: '#888', 
-    fontSize: 16
-  },
-  button: {
-    backgroundColor: "blue",
-    padding: 20,
-    borderRadius: 5,
-  },
-  buttonText: {
-    fontSize: 20,
-    color: '#fff',
-  },
-  thumbnail: {
-    width: 300,
-    height: 300,
-    resizeMode: "contain"
-  },
-  play: {
-    backgroundColor: "white",
-    padding: 20,
-    borderRadius: 5,
-    borderColor: '#000000',
-  },
-  controlText: {
-    fontSize: 20,
-    color: 'black',
+    backgroundColor: '#ecf0f1',
+    padding: 10,
   },
 });
