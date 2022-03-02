2f2fc1ebe4e9c1b058960de7aceb097104d467ba
markvakarchuk <vakaro12.mark@gmail.com>
2022-03-01 21:40:40 -0500

    changes to the new structure

--- app/nav/AppNavigator.js
+++ app/nav/AppNavigator.js
@@ -1,15 +1,18 @@
 //import liraries
 import React, { Component } from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import player from '../screens/player';
+import Player from '../screens/Player';
+import Settings from '../screens/Settings';
 import App from '../../App';
+// import { Settings } from 'react-native';
 
 const Tab = createBottomTabNavigator()
 
 // create a component
 const AppNavigator = () => {
     return <Tab.Navigator>
-        <Tab.Screen name='Player' component = {player} />
+        <Tab.Screen name='Player' component = {Player} />
+        <Tab.Screen name='Settings' component = {Settings} />
     </Tab.Navigator>
 }
 
